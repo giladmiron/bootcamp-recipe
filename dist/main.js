4c5d474ee235b3da18d9ee423a8d9920e41c4879
unknown <giladmiron98@gmail.com>
2019-07-04 12:09:11 +0300

    updated handlebars

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,11 @@
 function getRecipes() {
     let ingredient = $('.ingredientInput').val()
     $.get(`/recipes/${ingredient}`,function(response){
+        console.log(response)
         renderer.render(response)
     })
 }
+
+// $('body').on('click','img',function(){
+//     console.log()
+// })
